d0a3e065deafec623578fc499997e426a45c3398
Elitack <lzg960702@163.com>
2018-08-30 20:38:09 -0400

    adaption for CAB algorithm

--- BanditAlg/BanditAlgorithms_CAB.py
+++ BanditAlg/BanditAlgorithms_CAB.py
@@ -10,22 +10,22 @@ import networkx as nx
 import numpy as np
 from BanditAlg.BanditAlgorithms_LinUCB import *
 import collections
-class CABUserStruct(LinUCBUserStruct):
-    def __init__(self, featureDimension,  lambda_, userID):
-        LinUCBUserStruct.__init__(self,featureDimension = featureDimension, lambda_= lambda_, userID = userID)
+class CABArmStruct(LinUCBArmStruct):
+    def __init__(self, featureDimension,  lambda_, armID):
+        LinUCBArmStruct.__init__(self,featureDimension = featureDimension, lambda_= lambda_, armID = armID)
         self.reward = 0
         self.I = lambda_*np.identity(n = featureDimension)  
         self.counter = 0
         self.CBPrime = 0
         self.CoTheta= np.zeros(featureDimension)
         self.d = featureDimension
-        self.ID = userID
-        self.cluster = {}
+        self.ID = armID
+        self.cluster = []
     def updateParameters(self, articlePicked_FeatureVector, click):
         self.A += np.outer(articlePicked_FeatureVector,articlePicked_FeatureVector)
         self.b +=  articlePicked_FeatureVector*click
         self.AInv = np.linalg.inv(self.A)
-        self.UserTheta = np.dot(self.AInv, self.b)
+        self.ArmTheta = np.dot(self.AInv, self.b)
         self.counter+=1
         #print(self.CoTheta)
     def getCBP(self, alpha, article_FeatureVector,time):
@@ -47,79 +47,74 @@ class CABAlgorithm():
         self.FeatureDic = FeatureDic
         self.FeatureScaling = FeatureScaling
         self.feedback = 'edge'
-        self.users = {}  #Nodes
+        self.arms = {}  #Nodes
         self.currentP =nx.DiGraph()
         for u in self.G.nodes():
-            self.users[u] = CABUserStruct(dimension, lambda_, u)
             for v in self.G[u]:
+                self.arms[(u, v)] = CABArmStruct(dimension, lambda_, (u, v))
                 self.currentP.add_edge(u,v, weight=random())
-        n = len(self.users)
+        n = len(self.arms)
 
-        self.userIDSortedList = list(self.users.keys())
-        self.userIDSortedList.sort()
-        self.SortedUsers = collections.OrderedDict(sorted(self.users.items()))
+        self.armIDSortedList = list(self.arms.keys())
+        self.armIDSortedList.sort()
+        self.SortedArms = collections.OrderedDict(sorted(self.arms.items()))
 
         self.a=0
 
-    def decide(self):
+    def decide(self, feature_vec):
         self.time +=1
-        self.updateGraphClusters()
+        self.updateGraphClusters(feature_vec)
         S = self.oracle(self.G, self.seed_size, self.currentP)
         return S
 
-    def updateGraphClusters(self):
-        maxPTA = float('-inf')
-        articlePicked = None
-        for i in range(len(self.userIDSortedList)):
-            id_i = self.userIDSortedList[i]
-            WI = self.users[id_i].UserTheta
-            for (id_i, v) in self.G.edges(id_i):
-                clusterItem=[]
-                featureVector = self.FeatureScaling*self.FeatureDic[(id_i,v)]
-                CBI = self.users[id_i].getCBP(self.alpha, featureVector, self.time)
-                WJTotal=np.zeros(WI.shape)
-                CBJTotal=0.0
-                for j in range(len(self.users)):
-                    id_j = self.userIDSortedList[j]
-                    WJ = self.users[id_j].UserTheta
-                    CBJ = self.users[id_j].getCBP(self.alpha, featureVector, self.time)
-                    compare= np.dot(WI, featureVector) - np.dot(WJ, featureVector)               
-                    if (j != i):
-                        if (abs(compare) <= CBI + CBJ):
-                            clusterItem.append(self.users[id_j])
-                            WJTotal += WJ
-                            CBJTotal += CBJ
-                    else:    
-                        clusterItem.append(self.users[id_j])
-                        WJTotal += WI
-                        CBJTotal += CBI
-                CW= WJTotal/len(clusterItem)
-                CB= CBJTotal/len(clusterItem)
-                x_pta = np.dot(CW,featureVector) + CB
-                if x_pta > 1:
-                    x_pta = 1
-                self.currentP[id_i][v]['weight']  = x_pta
-                self.users[id_i].cluster[v] = clusterItem
+    def updateGraphClusters(self, feature_vec):
+        for i in range(len(self.armIDSortedList)):
+            id_i = self.armIDSortedList[i]
+            WI = self.arms[id_i].ArmTheta
+            clusterItem=[]
+            CBI = self.arms[id_i].getCBP(self.alpha, feature_vec, self.time)
+            WJTotal=np.zeros(WI.shape)
+            CBJTotal=0.0
+            for j in range(len(self.arms)):
+                id_j = self.armIDSortedList[j]
+                WJ = self.arms[id_j].ArmTheta
+                CBJ = self.arms[id_j].getCBP(self.alpha, feature_vec, self.time)
+                compare= np.dot(WI, feature_vec) - np.dot(WJ, feature_vec)               
+                if (j != i):
+                    if (abs(compare) <= CBI + CBJ):
+                        clusterItem.append(self.arms[id_j])
+                        WJTotal += WJ
+                        CBJTotal += CBJ
+                else:    
+                    clusterItem.append(self.arms[id_j])
+                    WJTotal += WI
+                    CBJTotal += CBI
+            CW= WJTotal/len(clusterItem)
+            CB= CBJTotal/len(clusterItem)
+            x_pta = np.dot(CW, feature_vec) + CB
+            if x_pta > 1:
+                x_pta = 1
+            self.currentP[id_i]['weight']  = x_pta
+            self.arms[id_i].cluster = clusterItem
 
-    def updateParameters(self, S, live_nodes, live_edges):
+    def updateParameters(self, S, live_nodes, live_edges, feature_vec):
         gamma = self.gamma
         for u in S:
             for (u, v) in self.G.edges(u):
-                featureVector = self.FeatureScaling*self.FeatureDic[(u,v)]
                 if (u,v) in live_edges:
                     reward = live_edges[(u,v)]
                 else:
                     reward = 0
-                if (self.users[u].getCBP(self.alpha, featureVector, self.time) >= gamma):
-                    self.users[u].updateParameters(featureVector, reward)
+                if (self.arms[(u, v)].getCBP(self.alpha, feature_vec, self.time) >= gamma):
+                    self.arms[(u, v)].updateParameters(feature_vec, reward)
                 else:
-                    clusterItem = self.users[u].cluster[v]
+                    clusterItem = self.arms[(u, v)].cluster
                     for i in range(len(clusterItem)):
-                        if(clusterItem[i].getCBP(self.alpha, featureVector, self.time) < gamma):
-                            clusterItem[i].updateParameters(featureVector, reward)
+                        if(clusterItem[i].getCBP(self.alpha, feature_vec, self.time) < gamma):
+                            clusterItem[i].updateParameters(feature_vec, reward)
 
-    def getLearntParameters(self, userID):
-        return self.users[userID].UserTheta
+    def getLearntParameters(self, armID):
+        return self.arms[armID].ArmTheta
         
     def getP(self):
         return self.currentP
