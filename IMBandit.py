75c8f8e45b5e7efd39c2ee00c0b350e7173fd345
Elitack <lzg960702@163.com>
2018-09-04 16:02:19 -0400

    CAB adapt to IMBandits

--- IMBandit.py
+++ IMBandit.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
     # algorithms['CLUB_0.2'] = CLUBAlgorithm(G, seed_size, oracle, dimension, alpha, alpha_2, lambda_, Feature_Dic, FeatureScaling, feedback = 'edge',  cluster_init="none")
     algorithms['CLUB_1'] = CLUBAlgorithm(G, seed_size, oracle, dimension, alpha, alpha_2, lambda_, FeatureScaling, feedback = 'edge',  cluster_init="none")
     # algorithms['CLUB_4'] = CLUBAlgorithm(G, seed_size, oracle, dimension, alpha, 4.0, lambda_, Feature_Dic, FeatureScaling, feedback = 'edge',  cluster_init="none")
-    # algorithms['CAB'] = CABAlgorithm(G, seed_size, oracle, dimension, alpha, alpha_2, lambda_, Feature_Dic, FeatureScaling, gamma)
+    algorithms['CAB'] = CABAlgorithm(G, seed_size, oracle, dimension, alpha, alpha_2, lambda_, FeatureScaling, gamma)
 
     simExperiment.runAlgorithms(algorithms)
 
